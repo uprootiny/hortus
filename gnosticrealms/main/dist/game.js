a2b6a5e051d1c3fe0378ff9eba9a7c8543d39cff
marchdown <marchdown@gmail.com>
2023-06-14 03:54:18 +0300

    Added new files to the git repo: futureflow/deps.edn

---
+++ gnosticrealms/main/dist/game.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+    var script = document.createElement('script');
+    script.src = 'https://raw.githubusercontent.com/uprootiny/GnosticRealms/main/dist/game.js';
+    document.body.appendChild(script);
+})();
